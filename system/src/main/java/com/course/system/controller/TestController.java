ef8ba270df3184842ecd8a3bf947bef9f038473a
CYF <996174599@qq.com>
2020-06-25 10:36:56 +0800

    1. mybatis使用示例：http://127.0.0.1:9001/system/test 2. 增加三层结构

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/test")
-    public String test(){
-        return "success";
+    @Resource
+    private TestService testService;
+
+    @RequestMapping(value = "/test",produces = {"application/json;charset=UTF-8"})
+    public List<Test> test(){
+        return testService.list();
     }
 }
