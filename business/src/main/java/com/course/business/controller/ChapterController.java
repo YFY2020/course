8c914fbf62501128d83e52ccbac3457e6bbadd8f
CYF <996174599@qq.com>
2020-07-01 10:11:39 +0800

    1.增加大章列表查询接口

---
+++ business/src/main/java/com/course/business/controller/ChapterController.java
@@ -0,0 +1,21 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Chapter;
+import com.course.server.service.ChapterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class ChapterController {
+
+    @Resource
+    private ChapterService chapterService;
+
+    @RequestMapping(value = "/chapter",produces = {"application/json;charset=UTF-8"})
+    public List<Chapter> chapter(){
+        return chapterService.list();
+    }
+}
