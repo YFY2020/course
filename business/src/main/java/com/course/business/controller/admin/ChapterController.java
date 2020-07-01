33db209ca9652e45232ef9edda1231d47522a0ae
CYF <996174599@qq.com>
2020-07-01 22:49:22 +0800

    1.集成axios完成前后端交互 2.增加CorsConfig，解决前后端跨域的问题

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,7 +1,6 @@
 package com.course.business.controller.admin;
 
-import com.course.server.domain.Chapter;
-import com.course.server.domain.dto.ChapterDto;
+import com.course.server.dto.ChapterDto;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +9,14 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/chapter")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
-    @RequestMapping(value = "/chapter",produces = {"application/json;charset=UTF-8"})
-    public List<ChapterDto> chapter(){
+    @RequestMapping(value = "/list",produces = {"application/json;charset=UTF-8"})
+    public List<ChapterDto> list(){
         return chapterService.list();
     }
 }
