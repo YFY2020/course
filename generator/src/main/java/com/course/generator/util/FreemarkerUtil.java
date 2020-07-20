01dd8b7630828e242fc81671cfcc4806abdbc260
CYF <996174599@qq.com>
2020-07-20 23:08:07 +0800

    1.生成器开发，增加生成service层的代码

--- generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
+++ generator/src/main/java/com/course/generator/util/FreemarkerUtil.java
@@ -9,6 +9,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class FreemarkerUtil {
 
@@ -23,10 +24,10 @@ public class FreemarkerUtil {
         temp = cfg.getTemplate(name);
     }
 
-    public static void generator(String filename) throws IOException, TemplateException {
+    public static void generator(String filename, Map<String,Object> map) throws IOException, TemplateException {
         FileWriter fw = new FileWriter(filename);
         BufferedWriter bw = new BufferedWriter(fw);
-        temp.process(null, bw);
+        temp.process(map, bw);
         bw.flush();
         fw.close();
     }
