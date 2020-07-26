b66f9749dccdaa785eca345b63d8eeb040dcdeec
CYF <996174599@qq.com>
2020-07-26 23:35:20 +0800

    1.课程管理与大章管理互相跳转

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -37,6 +37,9 @@
 
             <p>{{course.summary}}</p>
             <p>
+              <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
+                大章
+              </button>&nbsp;
               <button v-on:click="edit(course)" class="btn btn-white btn-xs btn-info btn-round">
                 编辑
               </button>&nbsp;
@@ -323,6 +326,15 @@
                         }
                     })
                 });
+            },
+
+            /**
+             * 点击【大章】
+             */
+            toChapter(course) {
+                let _this = this;
+                SessionStorage.set("course", course);
+                _this.$router.push("/business/chapter");
             }
         }
     }
