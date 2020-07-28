5a13beaab2f7ad302027926a5e93881049e4e1fc
CYF <996174599@qq.com>
2020-07-28 08:32:10 +0800

    1.保存小节时，更新课程总时长 2.增加自定义mapper

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -37,6 +37,11 @@
 
             <p>{{course.summary}}</p>
             <p>
+              <span class="badge badge-info">{{course.id}}</span>
+              <span class="badge badge-info">排序：{{course.sort}}</span>
+              <span class="badge badge-info">时长：{{course.time}}</span>
+            </p>
+            <p>
               <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
                 大章
               </button>&nbsp;
