e96c7b0186fe58baaf6c11d871448d76389bd365
CYF <996174599@qq.com>
2020-08-04 14:41:40 +0800

    1.文件上传功能开发，文件表设计与基本代码生成

--- admin/src/router.js
+++ admin/src/router.js
@@ -8,48 +8,52 @@ import Chapter from './views/admin/chapter.vue'
 import Section from './views/admin/section.vue'
 import Category from './views/admin/category.vue'
 import Teacher from './views/admin/teacher.vue'
-
+import File from './views/admin/file.vue'
 
 
 Vue.use(Router);
 
 export default new Router({
-    mode:'history',
-    base:process.env.BASE_URL,
-    routes:[{
-        path:'*',
-        redirect:"/login",
-    },{
-        path:'/login',
-        component:Login
-    },{
-        path:'/',
-        name:"admin",
-        component:Admin,
-        children:[{
-            path:'welcome',
-            name:"welcome",
-            component:Welcome,
-        },{
-            path:'business/category',
-            name:"business/category",
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+        path: '*',
+        redirect: "/login",
+    }, {
+        path: '/login',
+        component: Login
+    }, {
+        path: '/',
+        name: "admin",
+        component: Admin,
+        children: [{
+            path: 'welcome',
+            name: "welcome",
+            component: Welcome,
+        }, {
+            path: 'business/category',
+            name: "business/category",
             component: Category,
-        },{
-            path:'business/course',
-            name:"business/course",
+        }, {
+            path: 'business/course',
+            name: "business/course",
             component: Course,
-        },{
-            path:'business/chapter',
-            name:"business/chapter",
+        }, {
+            path: 'business/chapter',
+            name: "business/chapter",
             component: Chapter,
-        },{
-            path:'business/section',
-            name:"business/section",
+        }, {
+            path: 'business/section',
+            name: "business/section",
             component: Section,
-        },{
-            path:'business/teacher',
-            name:"business/teacher",
+        }, {
+            path: 'business/teacher',
+            name: "business/teacher",
             component: Teacher,
+        }, {
+            path: 'file/file',
+            name: "file/file",
+            component: File,
         }]
     }]
 })
