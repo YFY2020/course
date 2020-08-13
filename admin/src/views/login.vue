3013df7c5e16923a12ac10d4d787fc03fcd34518
CYF <996174599@qq.com>
2020-08-13 20:46:34 +0800

    1.用户管理与登录：增加记住登录信息功能（勾选记住我）

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -1,98 +1,117 @@
 <template>
-    <div class="main-container">
-        <div class="main-content">
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="login-container">
-                        <div class="center">
-                            <h1>
-                                <i class="ace-icon fa fa-leaf green"></i>
-                                <span class="">控台登录</span>
-                            </h1>
-                        </div>
+  <div class="main-container">
+    <div class="main-content">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="login-container">
+            <div class="center">
+              <h1>
+                <i class="ace-icon fa fa-leaf green"></i>
+                <span class="">控台登录</span>
+              </h1>
+            </div>
 
-                        <div class="space-6"></div>
+            <div class="space-6"></div>
 
-                        <div class="position-relative">
-                            <div id="login-box" class="login-box visible widget-box no-border">
-                                <div class="widget-body">
-                                    <div class="widget-main">
-                                        <h4 class="header blue lighter bigger">
-                                            <i class="ace-icon fa fa-coffee green"></i>
-                                            请输入用户名和密码
-                                        </h4>
+            <div class="position-relative">
+              <div id="login-box" class="login-box visible widget-box no-border">
+                <div class="widget-body">
+                  <div class="widget-main">
+                    <h4 class="header blue lighter bigger">
+                      <i class="ace-icon fa fa-coffee green"></i>
+                      请输入用户名和密码
+                    </h4>
 
-                                        <div class="space-6"></div>
+                    <div class="space-6"></div>
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
+                    <form>
+                      <fieldset>
+                        <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input v-model="user.loginName" type="text" class="form-control" placeholder="用户名" />
+															<input v-model="user.loginName" type="text" class="form-control" placeholder="用户名"/>
 															<i class="ace-icon fa fa-user"></i>
 														</span>
-                                                </label>
+                        </label>
 
-                                                <label class="block clearfix">
+                        <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input v-model="user.password" type="password" class="form-control" placeholder="密码" />
+															<input v-model="user.password" type="password" class="form-control" placeholder="密码"/>
 															<i class="ace-icon fa fa-lock"></i>
 														</span>
-                                                </label>
+                        </label>
 
-                                                <div class="space"></div>
+                        <div class="space"></div>
 
-                                                <div class="clearfix">
-                                                    <label class="inline">
-                                                        <input type="checkbox" class="ace" />
-                                                        <span class="lbl"> 记住我</span>
-                                                    </label>
+                        <div class="clearfix">
+                          <label class="inline">
+                            <input v-model="remember" type="checkbox" class="ace"/>
+                            <span class="lbl"> 记住我</span>
+                          </label>
 
-                                                    <button type="button"
-                                                            class="width-35 pull-right btn btn-sm btn-primary"
-                                                            v-on:click="login()">
-                                                        <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">登录</span>
-                                                    </button>
-                                                </div>
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  v-on:click="login()">
+                            <i class="ace-icon fa fa-key"></i>
+                            <span class="bigger-110">登录</span>
+                          </button>
+                        </div>
 
-                                                <div class="space-4"></div>
-                                            </fieldset>
-                                        </form>
+                        <div class="space-4"></div>
+                      </fieldset>
+                    </form>
 
-                                    </div><!-- /.widget-main -->
-                                </div><!-- /.widget-body -->
-                            </div><!-- /.login-box -->
-                        </div><!-- /.position-relative -->
-                    </div>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.main-content -->
-    </div><!-- /.main-container -->
+                  </div><!-- /.widget-main -->
+                </div><!-- /.widget-body -->
+              </div><!-- /.login-box -->
+            </div><!-- /.position-relative -->
+          </div>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.main-content -->
+  </div><!-- /.main-container -->
 </template>
 
 <script>
     export default {
         name: 'login',
-        data: function() {
+        data: function () {
             return {
                 user: {},
+                remember: true
             }
         },
-        mounted: function(){
+        mounted: function () {
+            let _this = this;
             $('body').removeClass('no-skin');
             $('body').attr('class', 'login-layout light-login');
+
+            let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER);
+            if (rememberUser) {
+                _this.user = rememberUser;
+            }
         },
-        methods:{
-            login () {
+        methods: {
+            login() {
                 let _this = this;
+                let passwordShow = _this.user.password;
+                _this.user.password = hex_md5(_this.user.password + KEY);
                 Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response) => {
                     Loading.hide();
                     let resp = response.data;
                     if (resp.success) {
-                        console.log("登录成功：",resp.content);
+                        console.log("登录成功：", resp.content);
+                        let loginUser = resp.content;
                         Tool.setLoginUser(resp.content);
+                        //判断是否勾选"记住我"
+                        if (_this.remember) {
+                            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
+                                loginName: loginUser.loginName,
+                                password: passwordShow
+                            });
+                        } else {
+                            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, null);
+                        }
                         _this.$router.push("/welcome")
                     } else {
                         Toast.warning(resp.message);
