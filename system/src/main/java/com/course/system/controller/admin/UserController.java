470cf4a50480967eee08663ca6d12c9a36000db2
CYF <996174599@qq.com>
2020-08-14 19:36:39 +0800

    1.用户管理与登录：集成redis，图片验证码的存储从session改为redis

--- system/src/main/java/com/course/system/controller/admin/UserController.java
+++ system/src/main/java/com/course/system/controller/admin/UserController.java
@@ -5,6 +5,7 @@ import com.course.server.service.UserService;
 import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +18,10 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
 
     private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
-    public static final String BUSINESS_NAME="";
+    public static final String BUSINESS_NAME="用户";
 
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Resource
     private UserService userService;
@@ -84,7 +87,9 @@ public class UserController {
         ResponseDto responseDto = new ResponseDto();
 
         // 根据验证码token去获取缓存中的验证码，和用户输入的验证码是否一致
-        String imageCode = (String) request.getSession().getAttribute(userDto.getImageCodeToken());
+        //String imageCode = (String) request.getSession().getAttribute(userDto.getImageCodeToken());
+        String imageCode = (String) redisTemplate.opsForValue().get(userDto.getImageCodeToken());
+
         if (StringUtils.isEmpty(imageCode)) {
             responseDto.setSuccess(false);
             responseDto.setMessage("验证码已过期");
