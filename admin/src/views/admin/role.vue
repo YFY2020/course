d985defac35aeaa67b714eea3baaf01094962914
CYF <996174599@qq.com>
2020-08-15 22:42:54 +0800

    1.通用权限管理：点击【关联用户】按钮时，加载所有用户

--- admin/src/views/admin/role.vue
+++ admin/src/views/admin/role.vue
@@ -1,105 +1,110 @@
 <template>
   <div>
-      <p>
-        <button v-on:click="add()"  class="btn btn-white btn-default btn-round">
-          <i class="ace-icon fa fa-edit"></i>
-          新增
-        </button>
-        &nbsp;
-        <button v-on:click="list(1)"  class="btn btn-white btn-default btn-round">
-          <i class="ace-icon fa fa-refresh"></i>
-          刷新
-        </button>
-      </p>
-      <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
-      <table id="simple-table" class="table  table-bordered table-hover">
-            <thead>
-            <tr>
-                                        <th>id</th>
-                        <th>角色</th>
-                        <th>描述</th>
-                <th>操作</th>
-            </tr>
-            </thead>
+    <p>
+      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
+        新增
+      </button>
+      &nbsp;
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
+    </p>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
+        <th>id</th>
+        <th>角色</th>
+        <th>描述</th>
+        <th>操作</th>
+      </tr>
+      </thead>
 
-            <tbody>
-            <tr v-for="role in roles">
-                            <td>{{role.id}}</td>
-                            <td>{{role.name}}</td>
-                            <td>{{role.desc}}</td>
-              <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                  <button v-on:click="editResource(role)" class="btn btn-xs btn-info">
-                    <i class="ace-icon fa fa-list bigger-120"></i>
-                  </button>
-                  <button v-on:click="edit(role)" class="btn btn-xs btn-info">
-                    <i class="ace-icon fa fa-pencil bigger-120"></i>
-                  </button>
+      <tbody>
+      <tr v-for="role in roles">
+        <td>{{role.id}}</td>
+        <td>{{role.name}}</td>
+        <td>{{role.desc}}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button v-on:click="editUser(role)" class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-user bigger-120"></i>
+            </button>
+            <button v-on:click="editResource(role)" class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-list bigger-120"></i>
+            </button>
+            <button v-on:click="edit(role)" class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
 
-                  <button v-on:click="del(role.id)" class="btn btn-xs btn-danger">
-                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                  </button>
-                </div>
+            <button v-on:click="del(role.id)" class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+          </div>
 
-                <div class="hidden-md hidden-lg">
-                  <div class="inline pos-rel">
-                    <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                      <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                    </button>
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+              </button>
 
-                    <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                      <li>
-                        <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+              <ul
+                class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
                                           <span class="blue">
                                             <i class="ace-icon fa fa-search-plus bigger-120"></i>
                                           </span>
-                        </a>
-                      </li>
+                  </a>
+                </li>
 
-                      <li>
-                        <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
                                           <span class="green">
                                             <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
                                           </span>
-                        </a>
-                      </li>
+                  </a>
+                </li>
 
-                      <li>
-                        <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
                                           <span class="red">
                                             <i class="ace-icon fa fa-trash-o bigger-120"></i>
                                           </span>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-      <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+              aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">表单</h4>
           </div>
           <div class="modal-body">
             <form class="form-horizontal">
-                         <div class="form-group">
-                             <label class="col-sm-2 control-label">角色</label>
-                             <div class="col-sm-10">
-                                 <input v-model="role.name" class="form-control">
-                             </div>
-                         </div>
-                         <div class="form-group">
-                             <label class="col-sm-2 control-label">描述</label>
-                             <div class="col-sm-10">
-                                 <input v-model="role.desc" class="form-control">
-                             </div>
-                         </div>
-             </form>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">角色</label>
+                <div class="col-sm-10">
+                  <input v-model="role.name" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">描述</label>
+                <div class="col-sm-10">
+                  <input v-model="role.desc" class="form-control">
+                </div>
+              </div>
+            </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
@@ -114,7 +119,8 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+              aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">角色资源关联配置</h4>
           </div>
           <div class="modal-body">
@@ -133,34 +139,93 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
+
+    <!-- 角色用户关联配置 -->
+    <div id="user-modal" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+              aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">角色用户关联配置</h4>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-md-6">
+                <table id="user-table" class="table table-hover">
+                  <tbody>
+                  <tr v-for="user in users">
+                    <td>{{user.loginName}}</td>
+                    <td class="text-right">
+                      <a v-on:click="addUser(user)" href="javascript:;" class="">
+                        <i class="ace-icon fa fa-arrow-circle-right blue"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-md-6">
+                <table id="role-user-table" class="table table-hover">
+                  <tbody>
+                  <tr v-for="user in roleUsers">
+                    <td>{{user.loginName}}</td>
+                    <td class="text-right">
+                      <a v-on:click="deleteUser(user)" href="javascript:;" class="">
+                        <i class="ace-icon fa fa-trash blue"></i>
+                      </a>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-white btn-default btn-round" data-dismiss="modal">
+              <i class="ace-icon fa fa-times"></i>
+              关闭
+            </button>
+            <button type="button" class="btn btn-white btn-info btn-round" v-on:click="saveUser()">
+              <i class="ace-icon fa fa-plus blue"></i>
+              保存
+            </button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 </template>
 
 <script>
     import Pagination from "../../components/pagination";
+
     export default {
         components: {Pagination},
         name: 'system-role',
-        data: function(){
+        data: function () {
             return {
                 role: {},
                 roles: [],
                 resources: [],
                 zTree: {},
+                user: [],
+                roleUsers: []
             }
         },
-        mounted: function(){
+        mounted: function () {
             let _this = this;
             _this.$refs.pagination.size = 5;
             _this.list(1)
             //sidebar激活样式 方法1
             //this.$parent.activeSidebar("system-role-sidebar");
         },
-        methods:{
+        methods: {
             /**
              * 点击【新增】
              */
-            add(){
+            add() {
                 let _this = this;
                 _this.role = {};
                 $("#form-modal").modal("show");
@@ -169,9 +234,9 @@
             /**
              * 点击【编辑】
              */
-            edit(role){
+            edit(role) {
                 let _this = this;
-                _this.role = $.extend({},role);
+                _this.role = $.extend({}, role);
                 $("#form-modal").modal("show");
             },
             /**
@@ -181,43 +246,43 @@
             list(page) {
                 let _this = this;
                 Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER+'/system/admin/role/list',{
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/role/list', {
                     page: page,
                     size: _this.$refs.pagination.size,
-                }).then((response)=>{
+                }).then((response) => {
                     Loading.hide();
-                    let resp  = response.data;
+                    let resp = response.data;
                     _this.roles = resp.content.list;
-                    _this.$refs.pagination.render(page,resp.content.total)
+                    _this.$refs.pagination.render(page, resp.content.total)
                 })
             },
 
             /**
              * 点击【保存】
              */
-            save(){
+            save() {
                 let _this = this;
 
                 // 保存校验
                 if (1 != 1
-                            || !Validator.require(_this.role.name, "角色")
-                            || !Validator.length(_this.role.name, "角色", 1, 50)
-                            || !Validator.require(_this.role.desc, "描述")
-                            || !Validator.length(_this.role.desc, "描述", 1, 100)
+                    || !Validator.require(_this.role.name, "角色")
+                    || !Validator.length(_this.role.name, "角色", 1, 50)
+                    || !Validator.require(_this.role.desc, "描述")
+                    || !Validator.length(_this.role.desc, "描述", 1, 100)
                 ) {
                     return;
                 }
 
                 Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER+'/system/admin/role/save',
-                    _this.role).then((response)=>{
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/role/save',
+                    _this.role).then((response) => {
                     let resp = response.data;
-                    if(resp.success){
+                    if (resp.success) {
                         Loading.hide();
                         $("#form-modal").modal("hide");
                         _this.list(1);
                         Toast.success("保存成功！")
-                    }else {
+                    } else {
                         Toast.warning(resp.message);
                     }
                 })
@@ -230,7 +295,7 @@
                 let _this = this;
                 Confirm.show("删除角色后不可恢复，确认删除？", function () {
                     Loading.show();
-                    _this.$ajax.delete(process.env.VUE_APP_SERVER+'/system/admin/role/delete/' + id).then((response) => {
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER + '/system/admin/role/delete/' + id).then((response) => {
                         Loading.hide();
                         let resp = response.data;
                         if (resp.success) {
@@ -257,7 +322,7 @@
             loadResource() {
                 let _this = this;
                 Loading.show();
-                _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/resource/load-tree').then((res)=>{
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/resource/load-tree').then((res) => {
                     Loading.hide();
                     let response = res.data;
                     _this.resources = response.content;
@@ -307,7 +372,7 @@
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/role/save-resource', {
                     id: _this.role.id,
                     resourceIds: resourceIds
-                }).then((response)=>{
+                }).then((response) => {
                     let resp = response.data;
                     if (resp.success) {
                         Toast.success("保存成功!");
@@ -322,7 +387,7 @@
              */
             listRoleResource() {
                 let _this = this;
-                _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/role/list-resource/' + _this.role.id).then((response)=>{
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/role/list-resource/' + _this.role.id).then((response) => {
                     let resp = response.data;
                     let resources = resp.content;
 
@@ -333,6 +398,34 @@
                         _this.zTree.checkNode(node, true);
                     }
                 });
+            },
+            /**
+             * 点击【用户】
+             */
+            editUser(role) {
+                let _this = this;
+                _this.role = $.extend({}, role);
+                _this.listUser();
+                $("#user-modal").modal("show");
+            },
+
+            /**
+             * 查询所有用户
+             */
+            listUser() {
+                let _this = this;
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/list', {
+                    page: 1,
+                    size: 9999
+                }).then((response)=>{
+                    let resp = response.data;
+                    if (resp.success) {
+                        _this.users = resp.content.list;
+                        _this.listRoleUser();
+                    } else {
+                        Toast.warning(resp.message);
+                    }
+                })
             },
         }
     }
