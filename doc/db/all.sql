bf5e420c9492594161e094155acd60bc49893cc7
CYF <996174599@qq.com>
2020-08-15 21:37:36 +0800

    1.通用权限管理：增加角色资源关联表，生成持久层和服务端代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -238,9 +238,24 @@ insert into role values ('00000000','系统管理员','管理用户、角色管�
 insert into role values ('00000001','开发','维护资源');
 insert into role values ('00000002','业务管理员','负责业务管理');
 
+-- 角色资源管理表
+drop table if exists role_resource;
+create table role_resource(
+    id char(8) not null default '' comment  'id',
+    role_id char(8) not null comment '角色|id',
+    resource_id char(6) not null comment '角色|id',
+    primary key (id)
+)engine=innodb default charset=utf8mb4 comment='角色资源关联';
 
-
-
+insert into role_resource values ('00000000','00000000','01');
+insert into role_resource values ('00000001','00000000','0101');
+insert into role_resource values ('00000002','00000000','010101');
+insert into role_resource values ('00000003','00000000','010102');
+insert into role_resource values ('00000004','00000000','010103');
+insert into role_resource values ('00000005','00000000','0102');
+insert into role_resource values ('00000006','00000000','010201');
+insert into role_resource values ('00000007','00000000','0103');
+insert into role_resource values ('00000008','00000000','010301');
 
 
 
