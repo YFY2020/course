f2107709824e1bdd86849b24813b93fab740aad5
CYF <996174599@qq.com>
2020-08-19 18:41:09 +0800

    1.课程详情页面开发，增加大章小节显示 使用d-none d-sm-inline组合，超小屏时隐藏，其他显示 v-for的第二变量是索引号

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -18,7 +18,8 @@
               <span class="price-now text-danger"><i class="fa fa-yen"></i>&nbsp;{{course.price}}&nbsp;&nbsp;</span>
             </p>
             <p class="course-head-button-links">
-              <a v-show="!memberCourse.id" v-on:click="enroll()" class="btn btn-lg btn-primary btn-shadow" href="javascript:;">立即报名</a>
+              <a v-show="!memberCourse.id" v-on:click="enroll()" class="btn btn-lg btn-primary btn-shadow"
+                 href="javascript:;">立即报名</a>
               <a v-show="memberCourse.id" href="#" class="btn btn-lg btn-success btn-shadow disabled">您已报名</a>
             </p>
           </div>
@@ -39,7 +40,6 @@
             </ul>
 
             <br>
-
             <!-- Tab panes -->
             <div class="tab-content">
               <div class="tab-pane active" id="info" v-html="course.content">
@@ -87,6 +87,7 @@
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   </main>
@@ -104,6 +105,7 @@
                 chapters: [],
                 sections: [],
                 COURSE_LEVEL: COURSE_LEVEL,
+                SECTION_CHARGE: SECTION_CHARGE
             }
         },
         mounted() {
@@ -114,12 +116,24 @@
         methods: {
             findCourse() {
                 let _this = this;
-                _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/find/' + _this.id).then((response)=>{
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/find/' + _this.id).then((response) => {
                     let resp = response.data;
                     _this.course = resp.content;
                     _this.teacher = _this.course.teacher || {};
                     _this.chapters = _this.course.chapters || [];
                     _this.sections = _this.course.sections || [];
+
+                    // 将所有的节放入对应的章中
+                    for (let i = 0; i < _this.chapters.length; i++) {
+                        let c = _this.chapters[i];
+                        c.sections = [];
+                        for (let j = 0; j < _this.sections.length; j++) {
+                            let s = _this.sections[j];
+                            if (c.id === s.chapterId) {
+                                c.sections.push(s);
+                            }
+                        }
+                    }
                 })
             },
         }
@@ -130,25 +144,79 @@
   /* 课程信息 */
   .course-head {
   }
+
   .course-head h1 {
     font-size: 2rem;
     margin-bottom: 1.5rem;
   }
+
   .course-head-item span {
     margin-right: 1rem;
   }
+
   .course-head-desc {
     font-size: 1rem;
     color: #555
   }
+
   .course-head a {
   }
+
   .course-head-price {
     font-size: 2rem;
   }
+
   @media (max-width: 700px) {
     .course-head h1 {
       font-size: 1.5rem;
+    }
+  }
+
+  /* 章节列表 */
+  .chapter {
+    padding-bottom: 1.25rem;
+  }
+
+  .chapter-chapter {
+    font-size: 1.25rem;
+    padding: 1.25rem;
+    background-color: #23527c;
+    color: white;
+    cursor: pointer;
+  }
+
+  .chapter-section-tr {
+    font-size: 1rem;
+  }
+
+  .chapter-section-tr td {
+    padding: 1rem 1.25rem;
+    vertical-align: middle;
+  }
+
+  /*鼠标手势*/
+  .chapter-section-tr td .section-title {
+    color: #555;
+  }
+
+  .chapter-section-tr td .section-title:hover {
+    color: #23527c;
+    font-weight: bolder;
+    cursor: pointer;
+  }
+
+  /*行头小图标*/
+  .chapter-section-tr td .section-title i {
+    color: #2a6496;
+  }
+
+  @media (max-width: 700px) {
+    .chapter-chapter {
+      font-size: 1.2rem;
+    }
+
+    .chapter-section-tr {
+      font-size: 0.9rem;
     }
   }
 </style>
