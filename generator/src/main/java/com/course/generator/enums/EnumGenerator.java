46a97b7092c50733e44849e347dc517a06b36d5e
CYF <996174599@qq.com>
2020-08-21 19:15:39 +0800

    1.会员登录注册功能开发，增加短信管理，生成代码后，需要配置资源权限

--- generator/src/main/java/com/course/generator/enums/EnumGenerator.java
+++ generator/src/main/java/com/course/generator/enums/EnumGenerator.java
@@ -22,6 +22,8 @@ public class EnumGenerator {
             toJson(CourseChargeEnum.class, bufferObject, bufferArray);
             toJson(CourseStatusEnum.class, bufferObject, bufferArray);
             toJson(FileUseEnum.class, bufferObject, bufferArray);
+            toJson(SmsUseEnum.class, bufferObject, bufferArray);
+            toJson(SmsStatusEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
